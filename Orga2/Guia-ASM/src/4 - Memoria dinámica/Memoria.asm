9e150bdbb929e6ed62d936a952421ffb9145ef3e
Thaiel <ttmarciale@gmail.com>
2025-04-18 20:23:34 -0300

    Hecho 4, falta el 5

--- Orga2/Guia-ASM/src/4 - Memoria dinámica/Memoria.asm
+++ Orga2/Guia-ASM/src/4 - Memoria dinámica/Memoria.asm
@@ -3,7 +3,7 @@ extern free
 extern fprintf
 
 section .data
-
+fmt_str: db "%s", 0   ; string de formato para fprintf
 section .text
 
 global strCmp
@@ -16,22 +16,122 @@ global strLen
 
 ; int32_t strCmp(char* a, char* b)
 strCmp:
-	ret
+	;a en rdi
+	;b en rsi
+	push rbp
+	mov rbp, rsp
+
+
+	.loop:
+		mov rdx, [rdi]	;Muevo cada carater a un registro de un byte
+		mov rcx, [rsi]
+
+		cmp dl, cl
+    	jl .ret_1         ; a < b
+    	jg .ret_minus1 
+		je .equal
+	
+	.equal:
+		cmp dl, 0
+		je .ret_0
+		inc rdi
+		inc rsi
+		jmp .loop
+
+
+	.ret_minus1:
+		mov eax, -1
+		jmp .fin
+
+	.ret_1:
+		mov eax, 1
+		jmp .fin
+
+	.ret_0:			
+		mov eax, 0
+		jmp .fin
+
+	.fin:
+		pop rbp
+		ret
 
 ; char* strClone(char* a)
 strClone:
-	ret
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14
+	push r15
+	mov r12, rdi	;preservo el valor de rdi
+	call strLen	
+	mov edi, eax
+	inc edi
+	mov r13d, edi	;preservo el len
+	call malloc		;rax tiene el puntero a la nueva memoria
+	mov r14, rax	;preservo para que rax apunte al primer caracter
+	mov r15, rax	;guardo rax en r15 porque r15 apunta al primer char
+	xor rax, rax
+	.loop_clone:
+		cmp r13d, 1
+		je fin_clone
+
+		mov al, [r12]
+		mov [r14], al
+		
+		inc r14
+		inc r12
+		dec r13d
+		jmp .loop_clone
+
+	fin_clone:
+		mov byte [r14], 0
+		mov rax, r15
+		pop r15
+		pop r14	
+		pop r13 
+		pop r12
+		pop rbp	
+		ret
 
 ; void strDelete(char* a)
 strDelete:
+	push rbp
+	mov rbp, rsp
+	call free
+	pop rbp
 	ret
 
 ; void strPrint(char* a, FILE* pFile)
 strPrint:
+	push rbp
+	mov rbp, rsp
+
+	mov rdx, rdi
+	mov rdi, rsi
+	mov rsi, [rel fmt_str]
+
+	call fprintf
+
+	pop rbp
 	ret
 
 ; uint32_t strLen(char* a)
 strLen:
-	ret
+	mov rsi, rdi
+	xor rdx, rdx
+	xor eax, eax
+	.loop_len:
+		mov dl, [rsi]
+
+		cmp dl, 0
+		je .fin_len
+
+		inc rsi
+		inc eax
+		jmp .loop_len
+
+	.fin_len:
+		ret
 
 
--- Orga2/Guia-ASM/src/4 - Memoria dinámica/main.c
+++ Orga2/Guia-ASM/src/4 - Memoria dinámica/main.c
@@ -3,11 +3,16 @@
 #include <ctype.h>
 #include <string.h>
 #include <assert.h>
-
+#include <malloc.h>
 #include "../test-utils.h"
 #include "Memoria.h"
 
 int main() {
 	/* Acá pueden realizar sus propias pruebas */
+	char* palabra = "Tato 21";
+	assert(strLen(palabra) == 7);
+	assert(strLen("") == 0);
+	assert(strCmp("sar", "23") == -1);
+	
 	return 0;
 }
